1023e170ce3fbbfe5cf3624a34aeaf49cbe31bdc
Bolaji Adesina <adesina.bolaji@yahoo.com>
2024-05-28 13:49:51 +0100

    staging todo app

---
+++ ModelAct/Activity.cs
@@ -0,0 +1,7 @@
+namespace Todo.ModelAct{
+    public class Activity
+    {
+        public string Description { get; set; }
+        public bool IsChecked { get; set; }
+    }
+}
---
+++ ViewModels/AddItemViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.Reactive;
+using ReactiveUI;
+using Todo.Models;
+
+namespace Todo.ViewModels
+{
+    class AddItemViewModel : ViewModelBase
+    {
+        string description;
+
+        public AddItemViewModel()
+        {
+            var okEnabled = this.WhenAnyValue(
+                x => x.Description,
+                x => !string.IsNullOrWhiteSpace(x));
+
+            Ok = ReactiveCommand.Create(
+                () => new TodoItem { Description = Description }, 
+                okEnabled);
+            Cancel = ReactiveCommand.Create(() => { });
+            Remove = ReactiveCommand.Create(() => { });
+        }
+
+        public string Description
+        {
+            get => description;
+            set => this.RaiseAndSetIfChanged(ref description, value);
+        }
+
+        public ReactiveCommand<Unit, TodoItem> Ok { get; }
+        public ReactiveCommand<Unit, Unit> Cancel { get; }
+
+        public ReactiveCommand<Unit, Unit> Remove { get; }
+    }
+}
